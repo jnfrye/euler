1c0d4504e6d59cc311b8ea64be53790d90aeab50
Josh Frye <jfrye@uoregon.edu>
2015-07-04 14:37:31 -0700

    more reorg (euler10)

--- euler10.py
+++ euler10.py
@@ -1,18 +1,9 @@
+import src.primes as primes
+
+
 # -----Euler Problem 10-----
 #
 # Find the sum of all the primes below two million.
 
 
-
-
-def prime_sum(limit=2000000):
-    # Returns sum of all primes that are less than <limit>
-
-    from sympy import sieve
-
-    s = 0
-
-    for p in sieve.primerange(1, limit):
-        s += p
-
-    return s
+print "The sum of all the primes below two million is", primes.prime_sum(2000000)
