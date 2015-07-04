c0efead86c55c012e63c2b30492ee57c07522201
Josh Frye <jfrye@uoregon.edu>
2015-07-04 12:38:50 -0700

    finished commenting

--- src/euler10.py
+++ src/euler10.py
@@ -2,24 +2,13 @@
 #
 # Find the sum of all the primes below two million.
 
-from sympy import sieve
+
 
 
 def prime_sum(limit=2000000):
+    # Returns sum of all primes that are less than <limit>
 
-
-    # TODO: FILL THIS OUT
-    #
-    # ARGUMENTS:
-    #           f_name (string)         name of file
-    #
-    # RETURNS:
-    #           lines (2D int list)     2D list of numbers in the file
-    #
-    # NOTES:
-    #           This will only work for files containing integers! (for now)
-    # TODO
-
+    from sympy import sieve
 
     s = 0
 
