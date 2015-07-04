e84d0aad6eeaf9bcea043c13ba8e76cd7cc0c3f1
Josh Frye <jfrye@uoregon.edu>
2015-07-04 12:20:50 -0700

    finished commenting

--- src/euler3.py
+++ src/euler3.py
@@ -6,19 +6,6 @@ import sympy
 
 def largest_prime_factor(limit):
 
-    # TODO: FILL THIS OUT
-    #
-    # ARGUMENTS:
-    #           f_name (string)         name of file
-    #
-    # RETURNS:
-    #           lines (2D int list)     2D list of numbers in the file
-    #
-    # NOTES:
-    #           This will only work for files containing integers! (for now)
-    # TODO
-
-
     p = sympy.primefactors(limit)
 
     return max(p)
