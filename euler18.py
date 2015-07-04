9a2810ef354f3348b56aa60ffc6826c3475c08a2
Josh Frye <jfrye@uoregon.edu>
2015-07-04 14:56:17 -0700

    final bit of reorg

--- euler18.py
+++ euler18.py
@@ -1,10 +1,24 @@
-import euler8
 import os
 
-tri = euler8.extract_numbers_from_file("euler18data.txt", str(os.getcwd()))
+import src.data_manip as data_manip
+
+# -----Euler Problem 18-----
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+#
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+#
+# That is, 3 + 7 + 4 + 9 = 23.
+# Find the maximum total from top to bottom of the triangle in euler18_data.txt
+
+tri = data_manip.extract_numbers_from_file("euler18_data.txt", str(os.getcwd()) + "/data")
 
 last_row = len(tri)
 
 for row in reversed(range(0, last_row)):
     for col in range(0, len(tri[row]) - 1):
         tri[row - 1][col] += max(tri[row][col], tri[row][col + 1])
+
+print "The maximum sum from top to bottom of the triangle in euler18_data.txt is", tri[0][0]
--- euler67.py
+++ euler67.py
@@ -1,10 +1,17 @@
-import euler8
 import os
 
-tri = euler8.extract_numbers_from_file("euler67data.txt", str(os.getcwd()))
+import src.data_manip as data_manip
+
+# -----Euler Problem 67-----
+# Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), 
+# a 15K text file containing a triangle with one-hundred rows.
+
+tri = data_manip.extract_numbers_from_file("euler67_data.txt", str(os.getcwd()) + "/data")
 
 last_row = len(tri)
 
 for row in reversed(range(0, last_row)):
     for col in range(0, len(tri[row]) - 1):
         tri[row - 1][col] += max(tri[row][col], tri[row][col + 1])
+
+print "The maximum sum from top to bottom of the triangle in euler67_data.txt is", tri[0][0]
